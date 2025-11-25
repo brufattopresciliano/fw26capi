5ad593feb914cef3ce085cee22509d67c0068a65
mariah <149427758+brufattopresciliano@users.noreply.github.com>
2025-11-25 17:19:03 -0400

    Add files via upload

---
+++ rotas/getnoticias.js
@@ -0,0 +1,20 @@
+module.exports = (app)=>{
+    app.get('/noticias', async (req, res)=>{
+    try{
+    await app.dbClient.connect();
+    const users = await app.dbClient.db('portalnoticias')
+    .collection('noticias')
+    .find()
+    .toArray();
+    res.json(users);
+    }catch(error){
+    await app.dbClient.close()
+    res.send("erro:"+error)
+    }
+    finally{
+        await app.dbClient.close()
+    }
+    })
+    }
+    
+    
--- src/index.js
+++ src/index.js
@@ -8,11 +8,20 @@ app.set('view engine','ejs')
 app.set('view','./views')
  // Define your routes
 
-app.get('/', (req, res) => {
-res.json({ message: 'Hello from Express on Vercel!' });
 
-});
-require("../rotas/home")
+
+
+require("../db/dbconnect")(app)
+require("../rotas/home")(app)
+
+require("../rotas/getnoticias")(app)
+require("../rotas/getidnoticias")(app)
+require("../rotas/gettiponoticias")(app)
+
+require("../rotas/postnoticias")(app)
+require("../rotas/putnoticias")(app)
+require("../rotas/delnoticias")(app)
+
 app.listen(port, () => {
 console.log(`Example app listening on port ${port}`);
 });
