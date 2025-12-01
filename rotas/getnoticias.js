7f1d09851b71a8fdf1bd9a035458b5e30c7f86ea
mariah <149427758+brufattopresciliano@users.noreply.github.com>
2025-12-01 16:56:39 -0400

    Add files via upload

--- rotas/getnoticias.js
+++ rotas/getnoticias.js
@@ -1,20 +1,6 @@
-module.exports = (app)=>{
-    app.get('/noticias', async (req, res)=>{
-    try{
-    await app.dbClient.connect();
-    const users = await app.dbClient.db('portalnoticias')
-    .collection('noticias')
-    .find()
-    .toArray();
-    res.json(users);
-    }catch(error){
-    await app.dbClient.close()
-    res.send("erro:"+error)
-    }
-    finally{
-        await app.dbClient.close()
-    }
+module.exports = (app) => {
+    app.get('/noticias', async (req, res) => {
+        res.send(" ol")
     })
-    }
-    
-    
+}
+
